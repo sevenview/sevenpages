05fe345ed97495de0931159313ecf12271a8a4a9
Steve Clarke <steve@sevenview.ca>
2013-01-27 20:33:54 -0330

    Create a prototype public-facing page

---
+++ app/controllers/sevenpages/public/pages_controller.rb
@@ -0,0 +1,10 @@
+module Sevenpages
+  module Public
+    class PagesController < ApplicationController
+      layout nil
+
+      def show
+      end
+    end
+  end
+end
--- spec/dummy/config/routes.rb
+++ spec/dummy/config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   root to: 'home#index'
 
   mount Sevenpages::Engine => "/sevenpages"
+
+  get ':slug', to: 'sevenpages/public/pages#show'
 end

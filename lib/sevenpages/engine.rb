cfb6244f7d2ac4b2d8dc6d28a145e93457c34286
Steve Clarke <steve@sevenview.ca>
2013-02-06 18:14:07 -0330

    Precompile CodeMirror assets so they're available in the app host

--- lib/sevenpages/engine.rb
+++ lib/sevenpages/engine.rb
@@ -2,6 +2,11 @@ module Sevenpages
   class Engine < ::Rails::Engine
     isolate_namespace Sevenpages
 
+    # Precompile assets
+    initializer "sevenpages.assets" do |app|
+      app.config.assets.precompile += ['codemirror/*']
+    end
+
     config.generators do |g|
       g.test_framework :rspec, fixture: false
       g.fixture_replacement :factory_girl, dir: 'spec/factories'

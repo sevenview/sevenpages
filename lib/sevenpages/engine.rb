a2b66797db1fd2168447b1dc57fa0192335bd893
Steve Clarke <steve@sevenview.ca>
2013-02-06 18:46:57 -0330

    Precompile assets - include the group: :all to ensure they get compiled even on Heroku with initialize_on_precompile set to false See http://timcardenas.com/how-to-develop-engines-that-compile-assets-on

--- lib/sevenpages/engine.rb
+++ lib/sevenpages/engine.rb
@@ -2,10 +2,22 @@ module Sevenpages
   class Engine < ::Rails::Engine
     isolate_namespace Sevenpages
 
-    # Precompile assets
-    initializer "sevenpages.assets" do |app|
-      app.config.assets.precompile += ['sevenpages/codemirror/*']
+    # Precompile assets - include the group: :all to ensure they get compiled
+    # even on Heroku with initialize_on_precompile set to false
+    # See http://timcardenas.com/how-to-develop-engines-that-compile-assets-on
+    initializer "sevenpages.assets", group: :all do |app|
+      app.config.assets.precompile += ['codemirror/*']
+      # Uncomment the lines below to view the names of assets as they are
+      # precompiled for the rails asset pipeline
+      def compile_asset?(path)
+        puts "Compiling: #{path}"
+        true
+      end
+
+      app.config.assets.precompile = [ method(:compile_asset?).to_proc ]
+
     end
+
 
     config.generators do |g|
       g.test_framework :rspec, fixture: false

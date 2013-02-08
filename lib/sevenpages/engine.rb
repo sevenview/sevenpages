526a9f49f90a72d7bb463c0dea73af9c7af8d6c6
Steve Clarke <steve@sevenview.ca>
2013-02-07 22:32:24 -0330

    Add CodeMirror files to precompile list

--- lib/sevenpages/engine.rb
+++ lib/sevenpages/engine.rb
@@ -2,15 +2,23 @@ module Sevenpages
   class Engine < ::Rails::Engine
     isolate_namespace Sevenpages
 
-    #initializer "sevenpages.assets", group: :all do |app|
-    #  #Uncomment the lines below to view the names of assets as they are
-    #  #precompiled for the rails asset pipeline
-    #  def compile_asset?(path)
-    #    puts "Compiling: #{path}"
-    #    true
-    #  end
-    #  app.config.assets.precompile = [ method(:compile_asset?).to_proc ]
-    #end
+    initializer "sevenpages.assets", group: :all do |app|
+      app.config.assets.precompile += [
+          'codemirror.css',
+          'codemirror.js',
+          'codemirror/*',
+          'sevenpages/codemirror_include.css',
+          'sevenpages/codemirror_include.js'
+      ]
+      
+      #Uncomment the lines below to view the names of assets as they are
+      #precompiled for the rails asset pipeline
+      #def compile_asset?(path)
+      #  puts "Compiling: #{path}"
+      #  true
+      #end
+      #app.config.assets.precompile = [ method(:compile_asset?).to_proc ]
+    end
 
     config.generators do |g|
       g.test_framework :rspec, fixture: false
